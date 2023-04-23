b171211b2ff51cf95ddeabab073ed5026c925f87
Edgar Santos <edgarrps@gmail.com>
2023-04-23 17:07:04 -0400

    entity data for base.go

--- domain/base.go
+++ domain/base.go
@@ -3,8 +3,8 @@ package domain
 import "time"
 
 type Base struct {
-	ID        string
-	CreatedAt time.Timer
-	UpdateAt  time.Timer
-	DeletedAt time.Timer
+	ID        string     `json:"id" gorm:"type:uuid;primary_key"`
+	CreatedAt time.Timer `json:"created_at" gorm:"type:datetime"`
+	UpdateAt  time.Timer `json:"udpated_at" gorm:"type:datetime"`
+	DeletedAt time.Timer `json:"deleted_at" gorm:"type:datetime"`
 }
